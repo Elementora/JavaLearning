b73c6352057e0843a32694a955cfbeaf76d68daf
Ersan <ersanaksahin@gmail.com>
2023-02-22 01:22:32 +0300

    Add files via upload

---
+++ VeriTipiDonusum.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ersan
+ */
+public class VeriTipiDonusum {
+    public static void main(String[] args)
+    {
+    
+        int yas = 26;
+        System.out.println(yas);
+        
+        String yas1 = Integer.toString(yas);
+        System.out.println(yas1);
+        
+        String yas2 = String.valueOf(yas);
+        System.out.println(yas2);
+        
+        String yas3 = "5"+yas;
+        System.out.println(yas3);
+        
+        
+        
+        String dogumYili = "1992";
+         System.out.println(dogumYili);
+        int dogumYili1 = Integer.parseInt(dogumYili);
+        System.out.println(dogumYili1);
+        
+        
+       int dogumYili2 = Integer.valueOf(dogumYili);
+       System.out.println(dogumYili2);
+        
+        int aileSiraNo = 27 ;
+        double doubleaileSiraNo = (double)aileSiraNo;
+        String stringAileSiraNo = String.valueOf(aileSiraNo);
+        double doubleaileSiraNo2 = Double.valueOf(stringAileSiraNo);
+        System.out.println(aileSiraNo);
+        System.out.println(doubleaileSiraNo);
+         System.out.println(doubleaileSiraNo2);
+        
+         
+         byte siraNo = 5 ;
+         short sinif = 10;
+         int yil =1992 ;
+         long tckn = 11111111111L;
+         
+         float kilo = 62f ;
+         double boy = 170;
+         
+         char karakterimiz ='p';
+         
+         
+         String stringSiraNo = String.valueOf(siraNo);
+         String stringSinif = String.valueOf(sinif);
+         String stringYil = String.valueOf(yil);
+         String stringTckn = String.valueOf(tckn);
+         
+         String stringKilo = String.valueOf(kilo);
+         String stringBoy = String.valueOf(boy);
+         String stringKarakterimiz = String.valueOf(karakterimiz);
+         
+         
+          System.out.println(stringSiraNo);
+           System.out.println(stringSinif);
+            System.out.println(stringYil);
+             System.out.println(stringTckn);
+              System.out.println(stringKilo);
+               System.out.println(stringBoy);
+                System.out.println(stringKarakterimiz);
+         
+         
+         
+         
+         
+         
+         
+                
+    }
+    
+}
