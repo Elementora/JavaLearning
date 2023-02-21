8daff850e21f36a39b7f1d0a966b5ceb3339d173
Ersan <ersanaksahin@gmail.com>
2023-02-22 01:21:12 +0300

    Add files via upload

---
+++ VeriTipiDegiskenler.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ersan
+ */
+public class VeriTipiDegiskenler {
+    
+    public static void main(String[] args)
+    {
+      boolean cevap  = false ;
+      System.out.println(cevap);
+      cevap = true;
+      System.out.println(cevap);
+      
+      
+      byte yas = 100;
+      System.out.println(yas);
+      
+      short rastgeleSayi = 5000;
+      System.out.println(rastgeleSayi);
+      
+      int ayniRakamliSayi = 111111111 ;
+      System.out.println(ayniRakamliSayi);
+      
+      long tckn = 11111111111L ;
+      System.out.println(tckn);
+      
+      float ondalikliSayi = 10.5F ;
+      System.out.println(ondalikliSayi);
+      
+      float ondalikSayi2 = 10;
+      System.out.println(ondalikSayi2);
+      
+      double buyukOndalikliSayi = 161984184343168479646431638494961919165135135412894.5D ;
+      System.out.println(buyukOndalikliSayi);
+      
+      char karakterDegiskeni = '3';
+      System.out.println(karakterDegiskeni);
+      
+      String kursAdi = "KapsamlıJavaEğitimi";
+      System.out.println(kursAdi);
+    
+    }
+    
+}
